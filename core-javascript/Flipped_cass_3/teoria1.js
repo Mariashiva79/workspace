49487dfdad71894e47cf3465acabd432eda6ae51
Mariashiva79 <mjimenezgorjon@gmail.com>
2024-09-27 20:41:57 +0200

    Comienzo ejercicios js

---
+++ core-javascript/Flipped_cass_3/teoria1.js
@@ -0,0 +1,44 @@
+//1. Write a JavaScript program that displays the largest integer among two integers.
+// function calculate_largest_integer(integer1, integer2) {
+//   let result;
+//   if (
+//     Number.isInteger(integer1) &&
+//     Number.isInteger(integer2) &&
+//     integer1 === integer2
+//   ) {
+//     result = `${integer1} is equal than ${integer2}`;
+//   } else if (Number.isInteger(integer1) && Number.isInteger(integer2)) {
+//     result =
+//       integer1 > integer2
+//         ? `${integer1} is largest than ${integer2}`
+//         : `${integer2} is largest than ${integer1}`;
+//   } else {
+//     result = `${integer1} and/or ${integer2} are not a integer number`;
+//   }
+//   return result;
+// }
+
+console.log(calculate_largest_integer(1, 2));
+console.log(calculate_largest_integer(5, 1));
+console.log(calculate_largest_integer(5, 1.5));
+
+console.log(calculate_largest_integer(5, 5));
+
+// opción 2
+function calculate_largest_integer(integer1, integer2) {
+  let result;
+  if (Number.isInteger(integer1) && Number.isInteger(integer2)) {
+    integer1 > integer2
+      ? (result = `${integer1} is largest than ${integer2}`)
+      : (result = `${integer2} is largest than ${integer1}`);
+    if (integer1 === integer2) result = `${integer1} is equal than ${integer2}`;
+  } else {
+    result = `${integer1} and/or ${integer2} are not a integer number`;
+  }
+  return result;
+}
+
+//2. Write a JavaScript conditional statement to find the sign of the product of three numbers. 
+//Display an alert box with the specified sign.
+//Sample numbers : 3, -7, 2
+//Output : The sign is -
