3e7470cd3296db26c27ba473dffc246564c81510
Mariashiva79 <mjimenezgorjon@gmail.com>
2024-09-29 14:15:36 +0200

    ejercicios tema 3

---
+++ core-javascript/Ejercicios js/ejercicios_tema3.js
@@ -0,0 +1,37 @@
+// 2. Crea una página web que te haga dos preguntas por medio del prompt y
+// que en caso de que respondas bien a las dos, te lo haga saber. De la misma
+// manera, si fallas en alguna te contestará con una negativa.
+
+// const question1 = prompt('What colour is the grass?').toLowerCase();
+// const question2 = prompt('What colour is the sky?').toLowerCase();
+
+// if (question1 === 'green' && question2 === 'blue') {
+//   alert(`Good job!`);
+// } else if (question1 !== 'green' && question2 === 'blue') {
+//   alert(`You failed the first question`);
+// } else if (question1 === 'green' && question2 !== 'blue') {
+//   alert(`You failed the second question`);
+// } else {
+//   alert(`Are not the answers I was looking for`);
+// }
+
+// 3. Crea una página web que te pregunte de qué quieres saber el precio. Puede
+// ser una tienda de ropa, un supermercado, una ferretería… En función de la
+// contestación del cliente te responde con el precio. Este ejercicio se valorará
+// más positivamente con un switch.
+// o Extra: ¿qué pasa con los idiomas, vas a escribir en castellano, inglés?
+// o Extra: ¿qué pasa con las mayúsculas, minúsculas?0
+
+// 4. Ejercicio
+// Crea un script que pida al usuario el diámetro de una rueda y su grosor (en
+// metros) y a través de condicionales if realice las siguientes operaciones:
+// a) Si el diámetro es superior a 1.4 debe mostrarse el mensaje “La rueda es para un
+// vehículo grande”. Si es menor o igual a 1.4 pero mayor que 0.8 debe mostrarse el
+// mensaje “La rueda es para un vehículo mediano”. Si no se cumplen ninguna de las
+// condiciones anteriores debe mostrarse por pantalla el mensaje “La rueda es para
+// un vehículo pequeño”.
+// b) Si el diámetro es superior a 1.4 con un grosor inferior a 0.4, ó si el diámetro es
+// menor o igual a 1.4 pero mayor que 0.8, con un grosor inferior a 0.25, deberá
+// mostrarse el mensaje “El grosor para esta rueda es inferior al recomendado”
+// Ejecuta el código y comprueba sus resultados. Para comprobar si es correcta tu
+// solución puedes consultar en los foros aprenderaprogramar.com.
