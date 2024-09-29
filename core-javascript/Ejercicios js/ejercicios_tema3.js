7dc03b0ef2bbbce37a8c42a556c2d686e0631626
Mariashiva79 <mjimenezgorjon@gmail.com>
2024-09-29 19:08:19 +0200

    ejercicios obligatorios

--- core-javascript/Ejercicios js/ejercicios_tema3.js
+++ core-javascript/Ejercicios js/ejercicios_tema3.js
@@ -35,3 +35,29 @@
 // mostrarse el mensaje “El grosor para esta rueda es inferior al recomendado”
 // Ejecuta el código y comprueba sus resultados. Para comprobar si es correcta tu
 // solución puedes consultar en los foros aprenderaprogramar.com.
+
+const diameter = parseFloat(
+  prompt('Please, enter the diameter of your whell, in meters'),
+  10,
+);
+const width = parseFloat(
+  prompt('Please, enter the width of your whell, in meters'),
+  10,
+);
+let wheel_size;
+if (diameter > 1.4) {
+  wheel_size = 'big';
+  alert(`The wheel is for a big car`);
+} else if (diameter <= 1.4 && diameter > 0.8) {
+  wheel_size = 'medium';
+  alert(`The wheel is for a medium car`);
+} else {
+  wheel_size = 'small';
+  alert(`The wheel is for a small car`);
+}
+if (
+  (wheel_size = 'big' && width < 0.4) ||
+  (wheel_size = 'medium' && width < 0.25)
+) {
+  alert(`The width for this wheel is less than recommended`);
+}
