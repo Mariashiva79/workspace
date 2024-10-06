c1e86754c957a50d48dc6bb2fd7662c6e4b248b3
Mariashiva79 <mjimenezgorjon@gmail.com>
2024-10-06 09:54:41 +0200

    empieza tema 6

---
+++ core-javascript/Tema_1/ejercicios/ejercicios_tema_1.js
@@ -0,0 +1,34 @@
+// Ejercicio 1
+let hora;
+const pi = 3.1416;
+
+// Ejercicio 2
+let numero;
+numero = 10;
+
+// Ejercicio 3
+let num1;
+let num2;
+let suma = num1 + num2;
+
+// Ejercicio 4
+let text1 = 'I love ';
+let text2 = 'my dogs';
+let suma_text = text1 + text2;
+console.log(suma_text);
+
+// Ejercicio 5
+class Dog {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    bark() {
+        console.log('Guau guau');
+    }
+}
+const mei = new Dog('Mei', 2);
+mei.bark();
+
+// Ejercicio 6
+// https://www.w3schools.com/js/exercise_js.asp?filename=exercise_js_variables1
