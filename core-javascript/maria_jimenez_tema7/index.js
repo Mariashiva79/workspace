0bb7c159973cea38210a67efff6df72ae301d581
Mariashiva79 <mjimenezgorjon@gmail.com>
2024-10-16 15:59:47 +0200

    ejercicio 3 html

--- core-javascript/maria_jimenez_tema7/index.js
+++ core-javascript/maria_jimenez_tema7/index.js
@@ -2,10 +2,14 @@
 // después el número del cuál se van a obtener múltiplos y devuelva un
 // array con el tamaño puesto de múltiplos de ese número (2, 4 => [4, 8])
 
-function array_of_multiples(size_array, choose_number_to_multiples) {
+const prompt = require('prompt-sync')();
+
+function array_of_multiples() {
+  const size_array = parseFloat(prompt('What is the size of the array?'));
+  const number_to_multiples = parseFloat(prompt('number to multiples?'));
   if (
     !Number.isInteger(size_array) ||
-    !Number.isInteger(choose_number_to_multiples) ||
+    !Number.isInteger(number_to_multiples) ||
     size_array < 0
   ) {
     throw new Error('Please, enter a valid number');
@@ -13,11 +17,13 @@ function array_of_multiples(size_array, choose_number_to_multiples) {
   const new_array = new Array(size_array);
 
   for (let i = 0; i < new_array.length; i++) {
-    new_array[i] = choose_number_to_multiples * (i + 1);
+    new_array[i] = number_to_multiples * (i + 1);
   }
   return new_array;
 }
 
-module.exports = { array_of_multiples };
+//array_of_multiples(user_size_array,user_number_to_multiples)
 
-// Mockear función para no llamar al input
+module.exports = {
+  array_of_multiples,
+};
